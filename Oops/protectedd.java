53d3d14be092d85a26822669770e6219b63dfeac
divishtk <divisht.avion@hotmail.com>
2021-05-24 15:33:38 +0530

    added protected

---
+++ Oops/protectedd.java
@@ -0,0 +1,26 @@
+package Oops;
+
+public class protectedd {
+   
+    
+
+        String name ="Divisht K";
+
+
+        protected void getName()
+        {
+
+
+
+            System.out.println("Name is "+name);
+
+
+        }
+
+
+
+}
+
+
+
+
---
+++ protected2/protected2.java
@@ -0,0 +1,21 @@
+package protected2;
+
+import Oops.protectedd;
+
+public class protected2  {
+    
+
+
+
+
+
+    public static void main(String[] args)
+    {
+
+
+        protectedd p=new protectedd();
+      //  p.getName(); //cannot access the protected method out side the pacakge but can access through inside class and package.compime time error
+        
+
+    }
+}
